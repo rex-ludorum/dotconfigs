573fe2f685725b4916d00eef1b408d779e116d50
Tony Lu <tony.lu@hp.com>
2023-12-06 15:22:10 -0600

    adding treesitter and removing rtp and packpath changes

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,17 +25,6 @@ function string:split(sSeparator, nMax, bRegexp)
 	return aRecord
 end
 
--- use .config folder for vim runtime and packages
-local splitRuntimePath = string.split(vim.o.runtimepath, ",")
-splitRuntimePath[1] = "~/.config/nvim"
-splitRuntimePath[#splitRuntimePath] = "~/.config/nvim/after"
-vim.o.runtimepath = table.concat(splitRuntimePath, ",")
-
-local splitPackPath = string.split(vim.o.packpath, ",")
-splitPackPath[1] = "~/.config/nvim"
-splitPackPath[#splitRuntimePath] = "~/.config/nvim/after"
-vim.o.packpath = table.concat(splitPackPath, ",")
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 -- NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -112,6 +101,14 @@ require("lazy").setup({
 				end,
 			},
 		},
+	},
+	{
+		-- Highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+			'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ':TSUpdate',
 	},
 }, {})
 
@@ -238,6 +235,90 @@ vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { de
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+
+-- [[ Configure Treesitter ]]
+-- See `:help nvim-treesitter`
+-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+vim.defer_fn(function()
+	require('nvim-treesitter.configs').setup {
+		-- Add languages to be installed here that you want installed for treesitter
+		ensure_installed = {
+			'c',
+			'cpp',
+			'go',
+			'lua',
+			'python',
+			'tsx',
+			'javascript',
+			'typescript',
+			'vimdoc',
+			'vim',
+			'bash',
+			'json',
+			'make',
+			'cmake',
+		},
+
+		-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+		auto_install = false,
+
+		highlight = { enable = true },
+		indent = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = '<c-space>',
+				node_incremental = '<c-space>',
+				scope_incremental = '<c-s>',
+				node_decremental = '<M-space>',
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					['aa'] = '@parameter.outer',
+					['ia'] = '@parameter.inner',
+					['af'] = '@function.outer',
+					['if'] = '@function.inner',
+					['ac'] = '@class.outer',
+					['ic'] = '@class.inner',
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					[']m'] = '@function.outer',
+					[']]'] = '@class.outer',
+				},
+				goto_next_end = {
+					[']M'] = '@function.outer',
+					[']['] = '@class.outer',
+				},
+				goto_previous_start = {
+					['[m'] = '@function.outer',
+					['[['] = '@class.outer',
+				},
+				goto_previous_end = {
+					['[M'] = '@function.outer',
+					['[]'] = '@class.outer',
+				},
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					['<leader>a'] = '@parameter.inner',
+				},
+				swap_previous = {
+					['<leader>A'] = '@parameter.inner',
+				},
+			},
+		},
+	}
+end, 0)
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
