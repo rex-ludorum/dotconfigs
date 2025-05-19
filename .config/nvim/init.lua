617cf7cb378898a00c66bde060940347eb597f2b
Tony Lu <tony.lu@hp.com>
2025-05-19 10:29:18 -0500

    update to nvim >= 0.11.1

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -394,17 +394,16 @@ local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
 	ensure_installed = vim.tbl_keys(servers),
-}
-
-mason_lspconfig.setup_handlers {
-	function(server_name)
-		require('lspconfig')[server_name].setup {
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = servers[server_name],
-			filetypes = (servers[server_name] or {}).filetypes,
-		}
-	end,
+	handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup {
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = servers[server_name],
+				filetypes = (servers[server_name] or {}).filetypes,
+			}
+		end,
+	},
 }
 
 -- [[ Configure nvim-cmp ]]
